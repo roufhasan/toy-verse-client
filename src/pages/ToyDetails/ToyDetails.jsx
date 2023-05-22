54954d6b18295d417a53c7f938e8aa6e939c2638
roufhasane <roufhasan5@gmail.comm>
2023-05-23 00:05:39 +0600

    toy detail page added

--- src/pages/ToyDetails/ToyDetails.jsx
+++ src/pages/ToyDetails/ToyDetails.jsx
@@ -1,11 +1,47 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 
 const ToyDetails = () => {
   const toy = useLoaderData();
-  const { name } = toy;
+  const {
+    name,
+    photoURL,
+    sellerName,
+    email,
+    price,
+    rating,
+    quantity,
+    details,
+  } = toy;
   return (
-    <div>
-      <h2>Toy Details{name}</h2>
+    <div className="mt-12 bg-[#F9DBBD] pt-[6%] rounded-lg">
+      <h1 className="text-4xl font-bold text-center pb-16 border-b-2 border-black">
+        Toy Details
+      </h1>
+      <div className="md:grid gap-12 grid-cols-6 bg-[#F9DBBD] p-[2%] rounded-lg">
+        <h2 className="text-2xl font-bold col-span-6">{name}</h2>
+        <div className="col-span-2">
+          <img src={photoURL} alt="" className="object-contain rounded-md" />
+        </div>
+        <div className="space-y-6 w-1/2 col-span-4">
+          <div className="space-y-2">
+            <p className="text-lg">Seller: {sellerName}</p>
+            <p className="text-lg">{rating}</p>
+            <Link className="text-lg" to={`mailto:${email}`}>
+              Contact: <span className="text-blue-500">{email}</span>
+            </Link>
+            <div className="divider"></div>
+          </div>
+          <div className="space-y-2">
+            <h3 className="text-3xl font-semibold">
+              <span className="text-red-500 font-normal text-2xl">-23% </span>$
+              {price}
+            </h3>
+            <p>In stock: {quantity}</p>
+            <div className="divider"></div>
+          </div>
+          <p>{details}</p>
+        </div>
+      </div>
     </div>
   );
 };
