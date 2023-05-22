18ff9123a1f4a1aac8b3347cb048f046a8983c50
roufhasane <roufhasan5@gmail.comm>
2023-05-23 00:24:42 +0600

    about us added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,3 +1,4 @@
+import AboutUs from "../AboutUs/AboutUs";
 import Banner from "../Banner/Banner";
 import Gallery from "../Gallery/Gallery";
 
@@ -6,6 +7,7 @@ const Home = () => {
     <div className="mt-12">
       <Banner></Banner>
       <Gallery></Gallery>
+      <AboutUs></AboutUs>
     </div>
   );
 };
